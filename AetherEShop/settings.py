16ae8cd318b52cae37c21552543712830eb73684
bezhan2009 <karimovbezan0@gmail.com>
2024-11-05 18:47:13 +0300

    [reft] changed DB

--- AetherEShop/settings.py
+++ AetherEShop/settings.py
@@ -93,22 +93,14 @@ WSGI_APPLICATION = 'AetherEShop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aether_new_main_db',
+        'NAME': 'aether_main_db',
         'USER': 'postgres',
         'PASSWORD': 'bezhan2009',
         'HOST': 'localhost',
         'PORT': '5432'
     }
 }
-'''
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
